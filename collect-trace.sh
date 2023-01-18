b9b066495b2c8c0c82dc0e37c311a3ef3565490c
Alex Semin <asemin@gradle.com>
2023-01-18 16:34:51 +0100

    Improve collect script

--- collect-trace.sh
+++ collect-trace.sh
@@ -8,22 +8,31 @@ DEFAULT_PROJECT="sample1-1app-1lib-1dep"
 DEFAULT_TASK="distZip"
 DEFAULT_TRACE_PREFIX="trace"
 GRADLE_CMD="${GRADLE_CMD:-./gradlew}"
+#!/usr/bin/env bash
+set -e
+
+# Usage: ./collect-trace.sh <project-dir> <task-name> <trace-file-prefix>
+
+PROJECT="${1:-$DEFAULT_PROJECT}"
+TASK="${2:-$DEFAULT_TASK}"
+TRACE_PREFIX="${3:-$DEFAULT_TRACE_PREFIX}"
+
+GRADLE_CMD="${GRADLE_CMD:-./gradlew}"
 
 # Function to print a command before running
 # shellcheck disable=SC2145
 exe() { echo ""; echo "\$ $@" ; "$@" ; }
 
-PROJECT="${1:-$DEFAULT_PROJECT}"
-cd "$(dirname "$0")/$PROJECT"
+cd "$PROJECT"
+exe pwd
 
-TASK="${2:-$DEFAULT_TASK}"
-TRACE_PREFIX="${3:-$DEFAULT_TRACE_PREFIX}"
 STORAGE_DIR="$PWD/_trace"
-EMPTY_GRADLE_HOME="fresh-gradle-home"
+EMPTY_GRADLE_HOME="${EMPTY_GRADLE_HOME:-fresh-gradle-home}"
 
 GRADLE_CMD="$GRADLE_CMD -g $EMPTY_GRADLE_HOME"
 
 # Make sure no local artifact transform results are present
+# shellcheck disable=SC2086
 exe $GRADLE_CMD --console=plain clean
 
 # Kill all Gradle daemons to make sure nothing is cached in memory
@@ -33,12 +42,13 @@ exe pkill -f "GradleDaemon $WRAPPER_VERSION" || true
 # Clean the temporary Gradle home to make sure artifact transform results are not cached on disk
 exe rm -rf "./$EMPTY_GRADLE_HOME/daemon"
 # Remove everything from caches except `modules-2` which contains downloaded dependencies
-exe find "./$EMPTY_GRADLE_HOME/caches" -mindepth 1 ! -name 'modules-2' -exec rm -rf {} +
+exe find "./$EMPTY_GRADLE_HOME/caches" -mindepth 1 -maxdepth 1 ! -name 'modules-2' -print -exec rm -rf {} + || true
 
 # Clean the storage dir
 exe rm -rf "$STORAGE_DIR"
 
 # Run task and collect build operations and build scan dump
+# shellcheck disable=SC2086
 exe $GRADLE_CMD --console=plain --no-build-cache "$TASK" \
   -Dorg.gradle.internal.operations.trace="$STORAGE_DIR/$TRACE_PREFIX" \
   --scan -Dscan.dump
@@ -48,7 +58,7 @@ exe $GRADLE_CMD --console=plain --no-build-cache "$TASK" \
 find . -name "*.scan" -maxdepth 1 -exec mv {} "$STORAGE_DIR" \;
 # Change build scan name to a simple name
 mv "$STORAGE_DIR"/*.scan "$STORAGE_DIR"/trace.scan
- 
+
 echo
 echo "Collected trace files:"
 find "$STORAGE_DIR"/*
